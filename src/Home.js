7c8d11f9be493031e252b50e31053d9bfe995593
karansaini2 <karansaini0868@gmail.com>
2022-06-28 20:18:07 +0530

    bugs fixed

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ const Home = ({ isAuth }) => {
       setBlogs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getBlogs();
-  });
+  }, [colRef]);
 
   return (
     <div className="homepage">
